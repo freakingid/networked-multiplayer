99a52c2c9de0395b224348e2426f83e6deea3219
Paul Kaiser <freakingid@github.com>
2017-03-21 17:11:36 -0500

    Start getting tanks to fire missiles

--- Assets/Scripts/MoveTank.cs
+++ Assets/Scripts/MoveTank.cs
@@ -1,30 +1,52 @@
-﻿using UnityEngine.Networking;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class MoveTank : NetworkBehaviour {
+public class MoveTank : NetworkBehaviour
+{
+
+	public GameObject missilePrefab;
+	public Transform endOfCanon;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 
-	public override void OnStartLocalPlayer()
+	public override void OnStartLocalPlayer ()
 	{
 		GetComponent<MeshRenderer> ().material.color = Color.green;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		if (!isLocalPlayer) {
 			return;
 		}
 
+		// Movement
 		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
 
 		transform.Rotate (0, x, 0);
 		transform.Translate (0, 0, z);
+
+		// Firing cannon keys
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			CmdFireMissile ();
+		}
+	}
+
+	// Actually fire the cannon
+	[Command]
+	void CmdFireMissile ()
+	{
+		var missile = (GameObject)Instantiate (missilePrefab, endOfCanon.position, endOfCanon.rotation);
+		missile.GetComponent<Rigidbody> ().velocity = missile.transform.forward * 6;
+		Destroy (missile, 2.0f);
+		NetworkServer.Spawn (missile);
 	}
 }
