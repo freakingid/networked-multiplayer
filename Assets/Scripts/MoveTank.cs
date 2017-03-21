c94bed2f58bd4e082f9b32baf1247a9f03ce318d
Paul Kaiser <freakingid@github.com>
2017-03-21 16:43:12 -0500

    Add basics of multiplayer networking

--- Assets/Scripts/MoveTank.cs
+++ Assets/Scripts/MoveTank.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using UnityEngine.Networking;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveTank : MonoBehaviour {
+public class MoveTank : NetworkBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,10 @@ public class MoveTank : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+
 		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
 
